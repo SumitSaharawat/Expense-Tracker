4e932ccc02a81c5cf3db3a513a972fcfd2c7faa8
SumitSaharawat <sumitsehrawat726@icloud.com>
2026-05-22 19:55:42 -0500

    finished login backend

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,14 +1,27 @@
 const userModel = require('../models/users.models');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '1d'});
+}
 
 const login = async (req, res) => {
-    res.json({message: 'login'})
+    const {email, password} = req.body;
+    try{
+        const user = await userModel.login(email, password);
+        const token = createToken(user._id);    
+        res.status(200).json({email, token});
+    }catch (error){
+        res.status(400).json({error: error.message});
+    }
 }
 
 const signup = async (req, res) => {
     const {email, password} = req.body;
     try{
         const user = await userModel.signup(email, password);
-        res.status(200).json(user);
+        const token = createToken(user._id);
+        res.status(200).json({email, token});
     }catch (error){
         res.status(400).json({error: error.message});
     }
--- backend/models/users.models.js
+++ backend/models/users.models.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 
-
-
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -41,5 +39,24 @@ userSchema.statics.signup = async function (email, password) {
 
     return user;
 };
+
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error('All fields must be filled');
+    }
+
+    const user = await this.findOne({ email });
+    if (!user) {
+        throw Error('Incorrect email');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        throw Error('Incorrect password');
+    }
+
+    return user;
+
+}
 
 module.exports = mongoose.model('User', userSchema);
